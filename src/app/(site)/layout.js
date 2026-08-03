beb78ee4b3271c545143ccc10fb54d0f1f1c196f
Moheed-Dar <moheeddar324@gmail.com>
2026-08-03 21:22:00 +0500

    updating new links, testimonial page, and text

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -2,7 +2,7 @@ import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 
 export const metadata = {
-  title: "Solid Rock Realty",
+  title: "Shelley Evelyn Real Estate - Your Trusted Real Estate Partner",
   description: "Premium Real Estate Services",
 };
 
